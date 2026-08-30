a9f5e0aab7e784e75a6d7be0ce8222b874ffa8ae
Yuri O. <yuri.dev44@outlook.com>
2026-08-30 11:59:00 -0400

    refactor: remove tanstack documentation comment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,7 +7,6 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
