e12553339719d6faff9a03815a101ce9a8846045
Yuri O. <yuri.dev44@outlook.com>
2026-08-30 11:57:37 -0400

    chore(deps): configure tanstack/router-plugin vite plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,16 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
