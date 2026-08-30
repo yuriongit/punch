4664ca3d9c66df14ea5bcc82560797413f34dad4
Yuri O. <yuri.dev44@outlook.com>
2026-08-30 19:06:38 -0400

    fix(web): correct ReactDOM client import using namespace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router"
 import { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
+import * as ReactDOM from "react-dom/client"
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
