9bf6521b4d8a029f985e1415fe62150ebd0e6037
Yuri Okeren <yuri.dev44@outlook.com>
2026-08-30 13:36:08 -0400

    chore(frontend): setup deps (#3)
    * fix: rename client to @apps/web
    
    * chore: update all deps and remove unused/unspecified deps from bun.lock
    
    * chore(deps): add biome
    
    * chore(deps): add tailwindcss
    
    * chore(deps): setup tailwindcss within vite.config
    
    * chore(deps): add tanstack/react-router
    
    * chore(deps): add tanstack/react-router devtools
    
    * chore(deps): add tanstack/router-plugin
    
    * chore(deps): configure tanstack/router-plugin vite plugin
    
    * refactor: remove tanstack documentation comment
    
    * chore(deps): add tanstack/react-router boilerplate
    
    * chore: add tanstack/react-router artifacts to .gitignore
    
    * chore(deps): add tanstack/router-cli

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,18 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
