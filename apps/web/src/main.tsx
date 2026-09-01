3a024dab358140cfd41d3bcece61b084c0b8a2ee
Yuri Okeren <yuri.dev44@outlook.com>
2026-08-31 20:56:42 -0400

    feat(web-pages): add home page (#11)
    * feat(web-fonts): add and use Excalifont
    
    * feat(web-fonts): update global font to Zalando Sans using Google Fonts
    
    * feat(web-fonts): add title and code font to Tailwind CSS @theme
    
    * fix(config-biome): enable css.parser.tailwindDirectives to resolve Biome lint error
    
    * chore(web-deps): add tabler icons
    
    * refactor(web-config): correct name in bun.lock
    
    * feat(web-pages): comlete home page
    
    * fix(web-build): resolve tanstack router invalid route error in attempt to build

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router"
 import { StrictMode } from "react"
 import * as ReactDOM from "react-dom/client"
+import "./style/index.css"
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
