c495aa37be031ca7078d2b0e23cacbf21002c23a
Yuri O. <yuri.dev44@outlook.com>
2026-09-01 12:14:02 -0400

    feat(animation): update create config page animation to slide from left

--- apps/web/src/routes/start/test/create-config.tsx
+++ apps/web/src/routes/start/test/create-config.tsx
@@ -3,6 +3,7 @@ import { createFileRoute, Link } from "@tanstack/react-router"
 import { AnimatePresence, motion, type Variants } from "motion/react"
 import { useState } from "react"
 import { JsonEditor } from "../../../components/JsonEditor"
+import { PAGE_SLIDE_FWD_VARIANTS } from "../../../constants/animations"
 
 export const Route = createFileRoute("/start/test/create-config")({
   component: RouteComponent,
@@ -71,87 +72,95 @@ function RouteComponent() {
   }
 
   return (
-    <main className="tracking-tight flex-col px-10 flex min-w-full items-center min-h-screen text-left justify-center py-10">
-      <div className="max-w-4xl w-full flex flex-col gap-5">
-        <div className="flex w-full justify-between items-end">
-          <div className="space-y-2">
-            <h1 className="text-3xl w-fit flex items-center justify-between font-bold">
-              Configure Punch
-            </h1>
-            <p className="italic font-code text-stone-600">punch.json</p>
+    <AnimatePresence mode="wait">
+      <motion.main
+        variants={PAGE_SLIDE_FWD_VARIANTS}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="tracking-tight flex-col px-10 flex min-w-full items-center min-h-screen text-left justify-center py-10"
+      >
+        <div className="max-w-4xl w-full flex flex-col gap-5">
+          <div className="flex w-full justify-between items-end">
+            <div className="space-y-2">
+              <h1 className="text-3xl w-fit flex items-center justify-between font-bold">
+                Configure Punch
+              </h1>
+              <p className="italic font-code text-stone-600">punch.json</p>
+            </div>
+            <button
+              type="button"
+              onClick={handleFormat}
+              className="border text-xs hover:cursor-pointer border-black/50 p-1.5 px-5 rounded-md flex items-center gap-1.5 w-fit text-black hover:bg-black/5 transition-all duration-300"
+            >
+              Format JSON
+            </button>
           </div>
-          <button
-            type="button"
-            onClick={handleFormat}
-            className="border text-xs hover:cursor-pointer border-black/50 p-1.5 px-5 rounded-md flex items-center gap-1.5 w-fit text-black hover:bg-black/5 transition-all duration-300"
-          >
-            Format JSON
-          </button>
-        </div>
-        <div className="border-2 bg-white border-purple-600/50 px-5 pt-9 rounded-md justify-center">
-          <div className="flex transition-all flex-col rounded overflow-hidden contain-content items-center">
-            {/* Validation Feedback */}
-            <JsonEditor
-              size={500}
-              jsonInput={jsonInput}
-              setJsonInput={handleJsonInputChange}
-            />
-            <AnimatePresence mode="wait">
-              {error && (
-                <motion.div
-                  key="error-banner"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={JSON_FORMAT_AND_ERRORS_VARIANTS}
-                  className="mb-10 absolute w-full max-w-208 flex justify-end"
-                >
-                  <div className="py-2 px-3.5 text-xs bg-red-500/15 text-red-700 border rounded-md border-red-500/15 font-code absolute w-fit max-w-2xs backdrop-blur-sm">
-                    <p className="font-bold">
-                      Invalid JSON:
-                      <span className="font-normal"> {error}</span>
-                    </p>
-                  </div>
-                </motion.div>
-              )}
-              {isFormatted && !error && (
-                <motion.div
-                  key="success-banner"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={JSON_FORMAT_AND_ERRORS_VARIANTS}
-                  className="mb-10 absolute w-full max-w-208 flex justify-end"
-                >
-                  <div className="py-2 px-3.5 text-xs bg-green-500/15 text-green-700 border rounded-md border-green-500/15 font-code absolute w-fit max-w-2xs backdrop-blur-sm">
-                    <p className="font-bold">
-                      Valid JSON:
-                      <span className="font-normal"> Formatted! </span>
-                    </p>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
+          <div className="border-2 bg-white border-purple-600/50 px-5 pt-9 rounded-md justify-center">
+            <div className="flex transition-all flex-col rounded overflow-hidden contain-content items-center">
+              {/* Validation Feedback */}
+              <JsonEditor
+                size={500}
+                jsonInput={jsonInput}
+                setJsonInput={handleJsonInputChange}
+              />
+              <AnimatePresence mode="wait">
+                {error && (
+                  <motion.div
+                    key="error-banner"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={JSON_FORMAT_AND_ERRORS_VARIANTS}
+                    className="mb-10 absolute w-full max-w-208 flex justify-end"
+                  >
+                    <div className="py-2 px-3.5 text-xs bg-red-500/15 text-red-700 border rounded-md border-red-500/15 font-code absolute w-fit max-w-2xs backdrop-blur-sm">
+                      <p className="font-bold">
+                        Invalid JSON:
+                        <span className="font-normal"> {error}</span>
+                      </p>
+                    </div>
+                  </motion.div>
+                )}
+                {isFormatted && !error && (
+                  <motion.div
+                    key="success-banner"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={JSON_FORMAT_AND_ERRORS_VARIANTS}
+                    className="mb-10 absolute w-full max-w-208 flex justify-end"
+                  >
+                    <div className="py-2 px-3.5 text-xs bg-green-500/15 text-green-700 border rounded-md border-green-500/15 font-code absolute w-fit max-w-2xs backdrop-blur-sm">
+                      <p className="font-bold">
+                        Valid JSON:
+                        <span className="font-normal"> Formatted! </span>
+                      </p>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <Link
+              to="/start/walkthrough/example-config"
+              className="border hover:cursor-pointer border-black/50 p-1.5 px-5 rounded-md flex items-center gap-1.5 w-fit text-black hover:bg-black/5 transition-all duration-300 hover:-rotate-z-2 hover:scale-105 active:scale-95"
+            >
+              <span>
+                <IconArrowBackUp stroke={1.5} />
+              </span>
+            </Link>
+            <Link
+              to="/"
+              // to="/start/test/punching"
+              className="hover:cursor-pointer p-1.5 px-5 rounded-md bg-purple-600 hover:bg-purple-500 text-white transition-all hover:rotate-z-3 hover:scale-105 gap-2 justify-center active:scale-95 w-fit duration-300 flex items-center uppercase font-bold pl-6"
+            >
+              Confirm <IconLockCheck stroke={2.75} size={21} />
+            </Link>
           </div>
         </div>
-        <div className="w-full flex justify-between items-center">
-          <Link
-            to="/start/walkthrough/example-config"
-            className="border hover:cursor-pointer border-black/50 p-1.5 px-5 rounded-md flex items-center gap-1.5 w-fit text-black hover:bg-black/5 transition-all duration-300 hover:-rotate-z-2 hover:scale-105 active:scale-95"
-          >
-            <span>
-              <IconArrowBackUp stroke={1.5} />
-            </span>
-          </Link>
-          <Link
-            to="/"
-            // to="/start/test/punching"
-            className="hover:cursor-pointer p-1.5 px-5 rounded-md bg-purple-600 hover:bg-purple-500 text-white transition-all hover:rotate-z-3 hover:scale-105 gap-2 justify-center active:scale-95 w-fit duration-300 flex items-center uppercase font-bold pl-6"
-          >
-            Confirm <IconLockCheck stroke={2.75} size={21} />
-          </Link>
-        </div>
-      </div>
-    </main>
+      </motion.main>
+    </AnimatePresence>
   )
 }
