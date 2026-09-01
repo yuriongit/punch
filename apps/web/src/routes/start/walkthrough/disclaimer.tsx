5c75055d6cdbc70b3755e985c6f342702eaa9d28
Yuri Okeren <yuri.dev44@outlook.com>
2026-08-31 21:25:26 -0400

    feat(web-page): add disclaimer page (#12)
    * feat(web-page): create disclaimer page
    
    * fix(web-build): resolve tanstack router invalid router error in build attempt
    
    * feat(web-pages): connect home page link button to next page

---
+++ apps/web/src/routes/start/walkthrough/disclaimer.tsx
@@ -0,0 +1,30 @@
+import { createFileRoute, Link } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/start/walkthrough/disclaimer")({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <main className="tracking-tight flex-col px-10 flex min-w-full items-center min-h-screen text-center justify-between py-10">
+      <div />
+      <div className="space-y-3 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">
+          Disclaimer:
+          <span className="text-purple-600"> Currently Under Development!</span>
+        </h1>
+        <p className="max-w-lg">
+          Punch is not yet an entirely feature-rich project. Although, that
+          doesn't mean that the core functionality and more isn't served :)
+        </p>
+      </div>
+      <Link
+        to="/"
+        // to="/start/walkthrough/example-config"
+        className="hover:cursor-pointer p-1.5 px-5 rounded-md bg-purple-600 hover:bg-purple-500 text-white transition-all hover:rotate-z-3 hover:scale-105 gap-2.5 justify-center active:scale-95 w-fit duration-300"
+      >
+        Okay
+      </Link>
+    </main>
+  )
+}
