aad0369f3bcbdd36f645809d9857d5ad5323126c
yuriongit <yuri.dev44@outlook.com>
2026-09-14 10:52:14 -0400

    docs(cli): replace package comment with updated comment

--- internal/cli/constants.go
+++ internal/cli/constants.go
@@ -1,4 +1,9 @@
-// Package cli implements the functionality of Punch's CLI tool. 
+// Package cli is the most important package for Punch's
+// CLI-tool as it implements the core functionality it
+// relies on. Disclaimer: Some of the types in
+// ./cli/types.go can seem to hold duplication. Despite,
+// package server and package cli sharing type and struct
+// names, they're completely different.
 package cli
 
 var wantStatusCode uint16 = 404
