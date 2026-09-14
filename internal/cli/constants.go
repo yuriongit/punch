009c8937a2f6b047ef1eb9a9dc89f94160168a76
yuriongit <yuri.dev44@outlook.com>
2026-09-13 21:54:30 -0400

    feat(cli): create and add /cli constants

---
+++ internal/cli/constants.go
@@ -0,0 +1,27 @@
+// Package cli implements the functionality of Punch's CLI tool. 
+package cli
+
+var wantStatusCode uint16 = 404
+var pWantStatusCode = &wantStatusCode
+var ClientData = ClientTestData{
+	TestID: "12CHARACTERS",
+	Config: PunchConfig{
+		Protocol: "http",
+		Target:   "localhost:3000/api/lilify/v1",
+		Children: []Child{
+			{
+				Name:             "/urls?alias=123456",
+				Method:           "GET",
+				TotalRequests:    28,
+				BaseDurationSecs: 4,
+				WantStatusCode:   pWantStatusCode,
+			},
+			{
+				Name:             "/urls?alias=bBKq8z",
+				Method:           "GET",
+				TotalRequests:    10,
+				BaseDurationSecs: 2,
+			},
+		},
+	},
+}
