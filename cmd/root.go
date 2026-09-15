2b93480fc517bf821676d52c258e2cef742e350b
yuriongit <yuri.dev44@outlook.com>
2026-09-15 14:03:26 -0400

    feat: add and setup Cobra

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Package cmd contains all of Punch's commands.
+
+----
+
+yuri.dev44@outlook.com | @yuriongit
+
+Copyright © 2026 Yuri Okeren
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "punch",
+	Short: "punch is a customizable, simple and lightweight HTTP load testing CLI tool.",
+	Long:  `TODO: Implement later`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.punch.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+Package main is the entry point of Punch.
+
+----
+
+yuri.dev44@outlook.com | @yuriongit
+
+Copyright © 2026 Yuri Okeren
+*/
+package main
+
+import "github.com/yuriongit/punch/cmd"
+
+func main() {
+	cmd.Execute()
+}
