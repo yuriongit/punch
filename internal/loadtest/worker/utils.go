cbfe2e05ab74fc9d675f1dbccd4e0d6ee8cba76b
yuriongit <yuri.dev44@outlook.com>
2026-09-17 21:35:42 -0400

    feat: implement colored logs; and extract stream-related helpers into utils.go

---
+++ internal/loadtest/worker/utils.go
@@ -0,0 +1,85 @@
+/*
+Package worker is responsible for carrying out the functionality
+of sending HTTP requests to the configured 'target' and 'children'
+in the Punch configuration file.
+relies on. Workers are able to make HTTP requests both in parallel
+and with concurrency.
+*/
+package worker
+
+import (
+	"fmt"
+	"time"
+
+	lg "charm.land/lipgloss/v2"
+)
+
+/*
+----------------------
+Stream-related helpers
+----------------------
+*/
+
+// streamPreTestLogs outputs initial benchmark headers.
+func streamPreTestLogs(
+	logChan chan<- string,
+	testID *string,
+) {
+	styledLeftBracket := primaryFaintStyle.Render("[ ")
+	styledRightBracket := primaryFaintStyle.Render(" ]")
+
+	logChan <- reverseLineBreak()
+	logChan <- lg.NewStyle().Render(fmt.Sprintf(
+		"%s%s Starting: %s%s",
+		styledLeftBracket,
+		faintStyle.Render("i."),
+		primaryStyle.Bold(true).Render(fmt.Sprintf("TEST-%s", *testID)),
+		styledRightBracket,
+	),
+	)
+	logChan <- lineBreak("normal")
+}
+
+// streamPostTestLogs outputs post test metrics.
+func streamPostTestLogs(
+	logChan chan<- string,
+	d *StreamPostTestData,
+	c *StreamPostTestLogsCounts,
+) {
+	/* Capture current time before date to acquire as accurate of a
+	result as possible. */
+	currentTime := time.Now().Format(time.TimeOnly)
+	currentDate := time.Now().Format(dateFormat)
+
+	// Create post test metrics.
+	var (
+		// Implement "Status" field's value. Will output "Error" or "Success"
+		// based off error counts
+		completedTestLog     = createPostMetricLog("Status", "UNIMPLEMENTED")
+		dateLog              = createPostMetricLog("Date", currentDate)
+		timeLog              = createPostMetricLog("Time", currentTime)
+		testIDLog            = createPostMetricLog("Test ID", fmt.Sprintf("TEST-%s", *d.TestID))
+		testDurationLog      = createPostMetricLog("Duration", formatResponseDuration(d.TestDur))
+		gracePeriodLog       = createPostMetricLog("Grace Period", fmt.Sprintf("%d%s", d.GracePeriodPercent, "%"))
+		fulfilledRequestsLog = createPostMetricLog("Fulfilled", fmt.Sprintf("%d requests", c.Global))
+		totalWorkersLog      = createPostMetricLog("Workers", fmt.Sprintf("%d workers", c.Workers))
+	)
+
+	// Stream post test metrics.
+	logChan <- lineBreak("")
+	logChan <- completedTestLog
+	logChan <- testIDLog
+	logChan <- testDurationLog
+	logChan <- gracePeriodLog
+	logChan <- fulfilledRequestsLog
+	logChan <- totalWorkersLog
+	logChan <- dateLog
+	logChan <- timeLog
+	logChan <- lineBreak("exit")
+}
+
+/*
+----------------------
+Stream-related helpers
+----------------------
+*/
